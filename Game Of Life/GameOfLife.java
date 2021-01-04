c90a9a950407fea3c285f0d2a4d1c302423641cf
kevin09090 <haha030921@gmail.com>
2021-01-03 16:38:02 -0800

    add 2D array & printBoard & initBoard

--- Game Of Life/GameOfLife.java
+++ Game Of Life/GameOfLife.java
@@ -1,3 +1,4 @@
+import java.util.*;
 
 /**
  * Game of Life Main class
@@ -7,8 +8,38 @@
  */
 public class GameOfLife {
     
+    private int row, col;
+    private boolean[][] board;
+    
     public GameOfLife() {
-       
+        this.row = 10;
+        this.col = 30;
+        this.board = new boolean[row][col];
+    }
+    
+    public void printBoard() {
+        for (int r = 0; r < row; ++r) {
+            for (int c = 0; c < col; ++c) {
+                System.out.print(board[r][c] ? "#" : " ");
+            }
+            System.out.println();
+        }
+    }
+    
+    public void initBoard(double ratio) {
+        Random random = new Random(System.currentTimeMillis());
+        
+        for (int r = 0; r < row; ++r) {
+            for (int c = 0; c < col; ++c) {
+                board[r][c] = random.nextDouble() < ratio;  
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        GameOfLife gameOfLife = new GameOfLife();
+        gameOfLife.initBoard(0.3); 
+        gameOfLife.printBoard(); 
     }
 }
 
